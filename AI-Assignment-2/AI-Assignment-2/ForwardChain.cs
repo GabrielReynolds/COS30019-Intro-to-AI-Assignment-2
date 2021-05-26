a9c025925bd378de761e50cb7c7a37d7149b3ad2
SAMUEL HAUSER <hausersm1@gmail.com>
2021-05-26 13:16:27 +1000

    Forward Chain start/added count to clause

--- AI-Assignment-2/AI-Assignment-2/ForwardChain.cs
+++ AI-Assignment-2/AI-Assignment-2/ForwardChain.cs
@@ -1,16 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AI_Assignment_2
-
-public class FowardChain : SearchMethods
 {
-    public ForwardChain()
+    public class ForwardChain : SearchMethods
     {
+        public ForwardChain(KnowledgeBase kb) : base(kb) { }
 
-    }
 
-    public override bool solve()
-    {
-        
+        public override bool solve(Clause query)
+        {
+
+            Stack<string> symbols = new Stack<string>();    // list for easy management
+            List<string> Inferred = new List<string>();
+            for (int i = 0; i < kb.Size; i++)
+            {
+                if (kb.Indexer(i).Proposition == null)
+                    symbols.Push(kb.Indexer(i).Value);
+                else
+                {
+                    foreach(string s in kb.Indexer(i).Proposition)
+                    {
+                        symbols.Push(s);
+                    }
+                   
+                }
+            }
+            while (symbols.Count != 0)
+            {
+                string p = symbols.Pop();
+                if (p == query.Value)
+                    return true;
+                if(!Inferred.Contains(p))
+                {
+                    Inferred.Add(p);
+                    foreach(Clause c in kb.list)
+                    {
+                        if(c.Proposition.Contains(p))
+                        {
+                            c.Count--;
+                            if (c.Count == 0)
+                                symbols.Push(c.Value);
+                        }
+                    }
+                }
+            }
+            return false ;
+        }
     }
 }
